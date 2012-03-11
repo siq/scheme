57de1a4c41466ce1a1d30e4363ea5e1ce7e75e72
Jordan McCoy <mccoy.jordan@gmail.com>
2012-03-10 21:18:58 -0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='scheme',
+    version='1.0.a1',
+    description='A schema framework.',
+    author='Jordan McCoy',
+    author_email='mccoy.jordan@gmail.com',
+    license='BSD',
+    url='http://github.com/siq/scheme',
+    packages=['scheme'],
+)
