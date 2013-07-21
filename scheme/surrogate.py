14b72fed7d4cf875daedb8ad22cf040c5b7fc677
Jordan McCoy <mccoy.jordan@gmail.com>
2013-07-21 17:32:04 -0500

    implemented support for surrogates with dynamic (self-contained) schemas

--- scheme/surrogate.py
+++ scheme/surrogate.py
@@ -1,16 +1,10 @@
+import scheme
 from scheme.interpolation import interpolate_parameters
 from scheme.util import identify_object, import_object
 
 class SurrogateMeta(type):
     def __new__(metatype, name, bases, namespace):
         cls = type.__new__(metatype, name, bases, namespace)
-        if cls.schema:
-            from scheme.fields import Structure, Token
-            if isinstance(cls.schema, Structure):
-                cls.schema.insert(Token(name='_', nonempty=True), True)
-            else:
-                raise TypeError(cls.schema)
-
         cls.surrogate = identify_object(cls)
         return cls
 
@@ -21,11 +15,19 @@ class surrogate(dict):
     cache = {}
     schema = None
 
+    def __init__(self, value, schema=None):
+        super(surrogate, self).__init__(value)
+        self._dynamic_schema = schema
+
     def __repr__(self):
         return '%s(%s)' % (self.surrogate, super(surrogate, self).__repr__())
 
+    @property
+    def effective_schema(self):
+        return self._dynamic_schema or self.schema
+
     @classmethod
-    def construct(cls, implementation, value=None, strict=False, **params):
+    def construct(cls, implementation=None, value=None, schema=None, strict=False, **params):
         """Constructs a surrogate instance from ``value``, using the surrogate type
         indicated by ``implementation``.
 
@@ -36,17 +38,25 @@ class surrogate(dict):
             ``dict`` or object instance from which to 
         """
 
-
+        if implementation is None:
+            implementation = cls
         if isinstance(implementation, basestring):
             implementation = cls._get_implementation(implementation)
+
+        surrogate_schema = schema
+        if surrogate_schema:
+            if implementation.schema:
+                raise Exception('cannot specify dynamic schema for surrogate with inherent schema')
+        else:
+            surrogate_schema = implementation.schema
 
         if value is not None:
             if isinstance(value, dict):
                 value = dict(value)
-                if implementation.schema:
-                    value = implementation.schema.extract(value)
-            elif not strict and implementation.schema:
-                value = implementation.schema.extract(value, strict=False)
+                if surrogate_schema:
+                    value = surrogate_schema.extract(value)
+            elif not strict and surrogate_schema:
+                value = surrogate_schema.extract(value, strict=False)
             else:
                 raise ValueError(value)
             if params:
@@ -57,7 +67,7 @@ class surrogate(dict):
             raise ValueError(value)
 
         implementation.contribute(value)
-        return implementation(value)
+        return implementation(value, schema)
 
     @classmethod
     def contribute(cls, value):
@@ -65,25 +75,41 @@ class surrogate(dict):
 
     @classmethod
     def interpolate(cls, value, parameters, interpolator=None):
-        implementation = cls._get_implementation(value.get('_'))
-        if implementation.schema:
-            return implementation.schema.interpolate(value, parameters, interpolator)
+        implementation = cls._get_implementation(value.pop('_', None))
+        if '__schema__' in value:
+            schema = scheme.Field.reconstruct(value.pop('__schema__'))
+            if schema:
+                value = schema.interpolate(value, parameters, interpolator)
+                return implementation(value, schema)
+            else:
+                raise ValueError(value)
+        elif implementation.schema:
+            value = implementation.schema.interpolate(value, parameters, interpolator)
+            return implementation(value)
         else:
-            return value # should probably best effort this
+            return implementation(value)
 
     def serialize(self):
-        value = dict(self, _=self.surrogate)
-        if self.schema:
+        """Serializes this surrogate."""
+
+        value = dict(self)
+        if self._dynamic_schema:
+            value = self._dynamic_schema.serialize(value)
+        elif self.schema:
             value = self.schema.serialize(value)
+
+        value['_'] = self.surrogate
+        if self._dynamic_schema:
+            value['__schema__'] = self._dynamic_schema.describe()
         return value
 
     @classmethod
     def unserialize(cls, value, ancestry=None):
-        implementation = cls._get_implementation(value.get('_'))
-        if implementation.schema:
+        implementation = cls._get_implementation(value.pop('_', None))
+        if '__schema__' in value:
+            return implementation._unserialize_dynamic_surrogate(value, ancestry)
+        elif implementation.schema:
             value = implementation.schema.unserialize(value, ancestry=ancestry)
-
-        value.pop('_', None)
         return implementation(value)
 
     @classmethod
@@ -100,3 +126,16 @@ class surrogate(dict):
 
         cls.cache[token] = implementation
         return implementation
+
+    @classmethod
+    def _unserialize_dynamic_surrogate(cls, value, ancestry=None):
+        schema = value.pop('__schema__', None)
+        if not schema:
+            raise ValueError(value)
+
+        schema = scheme.Field.reconstruct(schema)
+        if not schema:
+            raise ValueError(value)
+
+        value = schema.unserialize(value, ancestry=ancestry)
+        return cls(value, schema)
