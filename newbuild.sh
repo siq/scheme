ce44b4d22d65332717f80fc0800f5d5b439ceb39
siq-service <siq-service@eslabs.ibm.com>
2014-11-24 16:24:17 +0000

    --allow-empty

--- newbuild.sh
+++ newbuild.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 [[ ! -d $BUILDPATH/usr/lib/python2.6/site-packages ]] && mkdir -p $BUILDPATH/usr/lib/python2.6/site-packages
-python setup.py build #--release $REVISION --changelog="$CHANGELOG"
-python setup.py install --skip-build --root $BUILDPATH --install-lib=$BUILDPATH/usr/lib/python2.6/site-packages
+python setup.py build
+python setup.py install --skip-build --root $BUILDPATH --install-lib='/usr/lib/python2.6/site-packages'
